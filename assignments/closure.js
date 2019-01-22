db75a5b0e227af54d198e3d79b8136295397a639
Giacomo Benati <giacomobenati@mailbox.org>
2019-01-22 15:10:09 +0100

    (MVP) closures done

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,15 +1,37 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function sillyClosureOnNumbers () {
+  const zero = 0;
+  return function natural () {
+    const natural = "natural numbers";
+    return function integer () {
+      const integer = "integer numbers";
+      return function rational () {
+        const rational = "rational numbers";
+        return function irrational () {
+          const irrrational = "irrational"
+          return function real () {
+            const real = "real numbers"
+            console.log("It's a prank, bro!")
+          }   
+        }
+      }
+    }
+  }
+} 
+
 
 // ==== Challenge 2: Create a counter function ====
+// Return a function that when invoked increments and returns a counter variable.
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return () => ++count;
 };
+const newCounter = counter();
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
-
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
